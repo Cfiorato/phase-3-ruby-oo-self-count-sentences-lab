2587abf2bc39194679af9825c5d186688f8fe572
Cameron Fiorato <cameronclayfiorato@gmail.com>
2022-01-13 12:31:48 -0600

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).filter {|sentence| !sentence.empty?}.length
   end
 end
+
+puts "1. 2! 3? 4? 5! 6. ".count_sentences
